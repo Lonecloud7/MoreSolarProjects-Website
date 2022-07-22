f1f62e83f631d2a3e9521a9e5d7b71af537543be
Okeke Dumaga <okeke.dumaga@gmail.com>
2022-07-22 10:41:33 +0100

    avatar card created

--- src/components/AvatarCard/AvatarCard.js
+++ src/components/AvatarCard/AvatarCard.js
@@ -1,14 +1,24 @@
 import React from "react"
 import * as styles from "./AvatarCard.module.scss"
+import { Link } from "gatsby"
 
 const AvatarCard = ({ img }) => {
   return (
     <>
       <div className={styles.Avatar}>
         <div className={styles.Avatar__content}>
-          <div className={styles.Avatar__img}>
-            <img src={require("./avatar2.jpg").default} alt="" />
-          </div>
+          <Link to={"/Leadership"}>
+            <div className={styles.Avatar__imgContainer}>
+              <div
+                className={styles.Avatar__img}
+                style={{
+                  backgroundImage: `url(${require("./avatar2.jpg").default})`,
+                }}
+              ></div>
+              {/* <img src={require("./avatar2.jpg").default} alt="" style={{backgroundPosition:"center"}}/> */}
+            </div>
+          </Link>
+
           <div className={styles.Avatar__name}>
             <p>Lorem ipsum.</p>
           </div>
