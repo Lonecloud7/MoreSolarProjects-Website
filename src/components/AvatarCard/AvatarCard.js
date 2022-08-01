e865234ecec519c1cce71c94773386598c549073
Okeke Dumaga <okeke.dumaga@gmail.com>
2022-08-01 14:36:56 +0100

    board display code done

--- src/components/AvatarCard/AvatarCard.js
+++ src/components/AvatarCard/AvatarCard.js
@@ -4,43 +4,24 @@ import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
-const boardMembers = [
-  {
-    img: "https://marketingedge.com.image/wp-content/uploads/2022/02/E77ACD13-5B1E-43EC-B703-557419BACDE1.jpeg",
-    name: "Lorem ipsum.",
-    title: "CEO",
-  },
-  {
-    img: "https://marketingedge.com.ng/wp-content/uploads/2021/06/Dayo-Adefila.jpg",
-    name: "Lorem ipsum.",
-    title: "Accountant",
-  },
-  {
-    img: "https://nairametrics.com/wp-content/uploads/2021/11/Olu-Akanmu.jpg",
-    name: "Lorem ipsum.",
-    title: "General Counsel",
-  },
-  {
-    img: "../images/leadership/avatar5.jpg",
-    name: "Lorem ipsum.",
-    title: "Non-Executive Director",
-  },
-  {
-    img: "../images/leadership/avatar6.jpg",
-    name: "Lorem ipsum.",
-    title: "Executive Director, Commercial/CFO",
-  },
-  {
-    img: "../images/leadership/avatar7.jpg",
-    name: "Lorem ipsum.",
-    title: "Chairman",
-  },
-]
-
 const AvatarCard = () => {
   const data = useStaticQuery(graphql`
     query {
-      allFile {
+      allFile(
+        filter: {
+          name: {
+            nin: [
+              "contactus"
+              "heading"
+              "hero"
+              "hero2"
+              "leadership"
+              "operation"
+              "sustainability"
+            ]
+          }
+        }
+      ) {
         edges {
           node {
             base
@@ -53,36 +34,61 @@ const AvatarCard = () => {
     }
   `)
 
-  // const image = getImage(data.allFile.edges[0].node)
+  const boardMembers = [
+    {
+      img: data.allFile.edges[0].node,
+      name: "Lorem ipsum.",
+      title: "CEO",
+    },
+    {
+      img: data.allFile.edges[1].node,
+      name: "Lorem ipsum.",
+      title: "Accountant",
+    },
+    {
+      img: data.allFile.edges[2].node,
+      name: "Lorem ipsum.",
+      title: "General Counsel",
+    },
+    {
+      img: data.allFile.edges[3].node,
+      name: "Lorem ipsum.",
+      title: "Non-Executive Director",
+    },
+    {
+      img: data.allFile.edges[4].node,
+      name: "Lorem ipsum.",
+      title: "Executive Director, Commercial/CFO",
+    },
+    {
+      img: data.allFile.edges[5].node,
+      name: "Lorem ipsum.",
+      title: "Chairman",
+    },
+  ]
 
-  console.log(data)
+  //GRAPH QL PATH FOR IMAGE DISPLAY
+  // const image = getImage(data.allFile.edges[0].node)
   return (
     <>
       <div className={styles.Grid}>
         {boardMembers.map((BoardData, index) => {
           const { img, name, title } = BoardData
 
+          const image = getImage(img)
+
           return (
-            <div className={styles.Avatar} key={img}>
+            <div className={styles.Avatar} key={name}>
               <div className={styles.Avatar__content}>
                 <Link to={"/Leadership"}>
                   <div className={styles.Avatar__imgContainer}>
-                    {/* <img
-                      src={require("./avatar2.jpg").default}
+                    <GatsbyImage
+                      image={image}
+                      loading="eager"
                       alt=""
-                      style={{ backgroundPosition: "center" }}
-                    /> */}
-                    {data.allFile.edges.map((img, index) => {
-                      const image = getImage(img.node)
-                      return (
-                        <GatsbyImage
-                          image={image}
-                          loading="eager"
-                          alt=""
-                          className={styles.Avatar__img}
-                        />
-                      )
-                    })}
+                      className={styles.Avatar__img}
+                      key={index}
+                    />
                   </div>
                 </Link>
 
